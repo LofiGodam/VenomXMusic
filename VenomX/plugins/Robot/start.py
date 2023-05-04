6f30ba720b2b2009307d0670f977c73f7d419c39
LofiGodam <127813456+LofiGodam@users.noreply.github.com>
2023-05-04 23:55:58 +0530

    Update start.py

--- VenomX/plugins/Robot/start.py
+++ VenomX/plugins/Robot/start.py
@@ -42,8 +42,8 @@ async def start_comm(client, message: Message, _):
     if len(message.text.split()) > 1:
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
-            keyboard = help_pannel(_)
-            await message.reply_sticker("CAACAgUAAxkDAAEQ9xVkU_fBNE20XNZbmQ9oPmnCOeYfNQACfggAAhL0oFaPc7QOCXWJpS8E")
+            
+
             return await message.reply_photo(
                        photo=config.START_IMG_URL,
                        caption=_["help_1"], reply_markup=keyboard
